2f0dcdcadff17ebeb9f3b4707afdd8268850fb43
Hassanmir92 <hassanmir92@gmail.com>
2015-04-13 11:55:30 +0100

    Created associations between User, Game and Move

--- tictactoe/app/models/game.rb
+++ tictactoe/app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ActiveRecord::Base
+  has_many :moves
+  belongs_to :user1, class_name: 'User', foregin_key: 'user1_id'
+  belongs_to :user2, class_name: 'User', foregin_key: 'user2_id'
 end
--- tictactoe/db/schema.rb
+++ tictactoe/db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413100300) do
+ActiveRecord::Schema.define(version: 20150413100859) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "user1_id"
     t.integer  "user2_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "moves", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.integer  "value"
+    t.string   "character"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
