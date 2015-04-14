72ec3c2ca00931c0ce874d1c3459159e17466af9
Hassanmir92 <hassanmir92@gmail.com>
2015-04-14 21:44:04 +0100

    User can only make a move if it is his/her turn

--- tictactoe/app/models/move.rb
+++ tictactoe/app/models/move.rb
@@ -6,11 +6,17 @@ class Move < ActiveRecord::Base
 
   # validates :character, inclusion:{ in: POSSIBLE_CHARACTERS }
   validates :value, numericality: { only_integer: true, less_than: 9 }
+  validate :is_your_move?, on: :create
   # validates :value, uniqueness: true
   # validate :too_many_moves?
 
   # def too_many_moves?
   #   Game.moves.count < 10
   # end
+
+  def is_your_move?
+    errors.add(:user_id, "Wait your turn") unless game.whose_move?.id == user_id
+    game.reload
+   end
 
 end
