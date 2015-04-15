c5b4d8de14048010af536271670c93292bb64057
Hassanmir92 <hassanmir92@gmail.com>
2015-04-15 12:08:58 +0100

    lets user know when it is their turn.

--- tictactoe/app/models/move.rb
+++ tictactoe/app/models/move.rb
@@ -4,11 +4,5 @@ class Move < ActiveRecord::Base
   belongs_to :game
 
   validates :value, numericality: { only_integer: true, less_than: 9 }
-  validate :is_your_move?, on: :create
-
-  def is_your_move?
-    errors.add(:user_id, "Wait your turn") unless game.whose_move?.id == user_id
-    game.reload
-   end
 
 end
