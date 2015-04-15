2ec1f738fd9bba356b370d9a772f2e4c29e2991b
Hassanmir92 <hassanmir92@gmail.com>
2015-04-15 22:00:16 +0100

    Add basic styles and index+show pages for my user model

--- tictactoe/config/routes.rb
+++ tictactoe/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :games
   resources :moves, only: [:create]
   devise_for :users
+  resources :users, only: [:show, :index]
+
   root 'static_pages#home'
 end
