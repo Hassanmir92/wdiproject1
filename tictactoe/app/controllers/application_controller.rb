d5c86d91423a374c65a61a075fe944bc467667ee
Hassanmir92 <hassanmir92@gmail.com>
2015-04-16 13:21:11 +0100

    Users can now edit profile and it will save the changes

--- tictactoe/app/controllers/application_controller.rb
+++ tictactoe/app/controllers/application_controller.rb
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:email, :password, :password_confirmation, :username, :user_image)
     end
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :current_password, :username, :user_image)
+    end
   end
 end
