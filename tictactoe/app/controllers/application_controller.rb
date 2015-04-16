e52f4e3039eedcd84ce83e67c2488bdbb448edae
Hassanmir92 <hassanmir92@gmail.com>
2015-04-16 10:45:46 +0100

    Users can now show profile image on user index and show

--- tictactoe/app/controllers/application_controller.rb
+++ tictactoe/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:email, :password, :password_confirmation, :username)
+      u.permit(:email, :password, :password_confirmation, :username, :user_image)
     end
   end
 end
