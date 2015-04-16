d5f5951353d9c66d54724ecd438e691e3f17497f
Hassanmir92 <hassanmir92@gmail.com>
2015-04-16 16:15:13 +0100

    Added transparency to images and added a background

--- tictactoe/app/helpers/games_helper.rb
+++ tictactoe/app/helpers/games_helper.rb
@@ -3,8 +3,8 @@ module GamesHelper
     won = @game.game_won
     current_user_plays = @game.whose_move.id == current_user.id
     response = case 
-    when won && current_user_plays then  image_tag('lose.png', width: '100', alt: "LOSER!") 
-    when won && !current_user_plays then  image_tag("win.png", width: '100', alt: "WINNER!")
+    when won && current_user_plays then  image_tag('lose.png', width: '200', alt: "LOSER!") 
+    when won && !current_user_plays then  image_tag("win2.gif", width: '200', alt: "WINNER!")
     when @game.game_draw then "DRAW"
     end
     content_tag :h1, response
