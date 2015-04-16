e003affd3eb55488b1259e4e3c941a99a4b450b4
Hassanmir92 <hassanmir92@gmail.com>
2015-04-16 15:32:26 +0100

    Adding some styling

--- tictactoe/app/helpers/games_helper.rb
+++ tictactoe/app/helpers/games_helper.rb
@@ -3,8 +3,8 @@ module GamesHelper
     won = @game.game_won
     current_user_plays = @game.whose_move.id == current_user.id
     response = case 
-    when won && current_user_plays then  "LOSER!"
-    when won && !current_user_plays then  "WINNER"
+    when won && current_user_plays then  image_tag('lose.png', width: '100', alt: "LOSER!") 
+    when won && !current_user_plays then  image_tag("win.png", width: '100', alt: "WINNER!")
     when @game.game_draw then "DRAW"
     end
     content_tag :h1, response
